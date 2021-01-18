54290813594400c1579115a8df04a3f165ae0ed7
Aetrix27 <davedogg323@gmail.com>
2021-01-18 15:04:15 -0800

    Increased Efficiency

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-graphicsCardUrls = ['https://www.amazon.com/EVGA-08G-P5-3767-KR-GeForce-Technology-Backplate/dp/B08L8L9TCZ/ref=sr_1_1?dchild=1&keywords=3070&qid=1610223381&s=electronics&sr=1-1',
-                    'https://www.amazon.com/MSI-GeForce-Tri-Frozr-Architecture-Graphics/dp/B08HR7SV3M/ref=sr_1_3?crid=1Y1DLX8V0GXII&dchild=1&keywords=3080&qid=1610406031&s=electronics&sprefix=3080%2Celectronics%2C280&sr=1-3']
+graphicsCardUrls = ['https://www.amazon.com/EVGA-08G-P5-3767-KR-GeForce-Technology-Backplate/dp/B08L8L9TCZ/ref=sr_1_1?dchild=1&keywords=3070&qid=1610223381&s=electronics&sr=1-1']
 
 urlPs5 = 'https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG?ref_=ast_sto_dp'
 xboxUrls = ['https://www.amazon.com/Xbox-X/dp/B08H75RTZ8/ref=sr_1_13?dchild=1&keywords=xbox+series+x&qid=1610417662&sr=8-13',
@@ -62,7 +61,7 @@ def addDictValues(context, product, loopNum, itemInfo, name):
 
     return context, itemInfo
 
-productArr = [getItemInfo(graphicsCardUrls[0]), getItemInfo(graphicsCardUrls[1])]
+productArr = [getItemInfo(graphicsCardUrls[0])]
 product3 = getItemInfo(urlPs5)
 xboxItemArr = [getItemInfo(xboxUrls[0]), getItemInfo(xboxUrls[1])]
 
